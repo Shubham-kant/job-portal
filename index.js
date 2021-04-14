020637074ec4f210e5173fe08205e9525b4aa9ee
Shubham-Kant <pro.shubham1999@gmail.com>
2021-04-14 19:51:42 +0530

    added partials,layouts and static files

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express =require('express');
 const port=8000;
+
 const app=express();
+//layout
+const expressLayouts=require('express-ejs-layouts');
+app.use(express.static('assets'));
+app.use(expressLayouts);
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 app.use('/',require('./routes'));
 
 //set up the view engine
