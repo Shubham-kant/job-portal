8545377aa6df318732e5c78171ca5a8df0839d98
Shubham-Kant <pro.shubham1999@gmail.com>
2021-04-14 21:21:24 +0530

    added schema of candidate,recruiter and job

---
+++ models/job.js
@@ -0,0 +1,34 @@
+const mongoose=require('mongoose');
+
+const jobSchema=new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    isActive:{
+        type:Boolean,
+        required:true
+
+    },
+    //array of candidates
+    candidate:[{
+        type:mongoose.Schema.Types.ObjectId,
+        //name of model to be linked
+        ref:'Candidate'
+    }],
+    recruiter:{
+        type:mongoose.Schema.Types.ObjectId,
+        //name of model to be linked
+        ref:'Recruiter'
+    }
+},{
+    //to add a feature of 'created at' and 'updated at'.
+    timestamps:true
+});
+
+
+
+
+
+const Job=mongoose.model('Job',jobSchema);
+module.exports=Job;
