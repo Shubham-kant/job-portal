521957e84cf3f69d1ab2c180ba555a77fba1637d
Shubham-Kant <pro.shubham1999@gmail.com>
2021-04-15 14:34:01 +0530

    added recruiter sign out option

--- routes/recruiter.js
+++ routes/recruiter.js
@@ -6,7 +6,7 @@ const recruiterController=require('../controllers/recruiter_controller.js');
 router.get('/sign-up',recruiterController.signUp);
 router.get('/profile',passport.checkAuthentication,recruiterController.profile)
 router.get('/sign-in',recruiterController.signIn);
-
+router.get('/sign-out',recruiterController.destroySession);
 router.post('/create',recruiterController.create);
 // use passport as a middleware to authenticate
 router.post('/create-session',passport.authenticate(
